9936f0e9f2520718a55accfd1105e68aaeb1d4b1
Aidan Grigg <aidangrigg02@gmail.com>
2025-11-18 10:42:43 +1100

    crkbd changes

--- keyboards/crkbd/keymaps/aidangrigg/rules.mk
+++ keyboards/crkbd/keymaps/aidangrigg/rules.mk
@@ -1,26 +1,4 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
-CAPS_WORD_ENABLE = yes
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
-BOOTLOADER		= qmk-dfu
-
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
-LTO_ENABLE = yes
-SPACE_CADET_ENABLE = no
-GRAVE_ESC_ENABLE = no
-MAGIC_ENABLE  = no
-
-# If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./burst.c \
-        ./navi_logo.c \
-        ./gui_state.c \
-        ./fast_random.c \
-        ./layer_frame.c \
-        ./ring.c \
-        ./boot.c \
-        ./draw_helper.c \
-
+OLED_ENABLE      = yes
+OLED_DRIVER      = ssd1306
+BOOTLOADER	     = qmk-dfu
+COMBO_ENABLE     = yes
